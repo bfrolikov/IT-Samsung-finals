126548f67af01764c38de2ea89edfe1b3ec59746
bfrolikov <b.frolikov@yandex.ru>
2019-02-28 21:26:24 +0300

    SearchView works

---
+++ app/src/main/java/com/example/bfrol/it_samsung_finals/SearchFragment.java
@@ -0,0 +1,16 @@
+package com.example.bfrol.it_samsung_finals;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class SearchFragment extends Fragment {
+    //this fragment will be handling search for users
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_search,container,false);
+    }
+}
