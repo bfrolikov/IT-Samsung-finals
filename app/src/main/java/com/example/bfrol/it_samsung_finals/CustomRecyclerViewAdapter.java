9829405c06a4d2e5e191968b62041654594d72d6
boris <b.frolikov@yandex.ru>
2019-03-03 15:37:19 +0300

    added some files

---
+++ app/src/main/java/com/example/bfrol/it_samsung_finals/CustomRecyclerViewAdapter.java
@@ -0,0 +1,23 @@
+package com.example.bfrol.it_samsung_finals;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+
+public class CustomRecyclerViewAdapter extends RecyclerView.Adapter<CustomViewHolder> {
+    @NonNull
+    @Override
+    public CustomViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull CustomViewHolder customViewHolder, int i) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
--- app/src/main/java/com/example/bfrol/it_samsung_finals/MainActivity.java
+++ app/src/main/java/com/example/bfrol/it_samsung_finals/MainActivity.java
@@ -112,3 +112,4 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 }
+//TODO 1 look up firebase
