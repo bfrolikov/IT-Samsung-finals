568dd652334a3c296ae61b55c8363711ecab5448
boris <b.frolikov@yandex.ru>
2019-03-09 21:05:24 +0300

    marked the registration activity

--- app/src/main/java/com/example/bfrol/it_samsung_finals/LoginActivity.java
+++ app/src/main/java/com/example/bfrol/it_samsung_finals/LoginActivity.java
@@ -1,11 +1,15 @@
 package com.example.bfrol.it_samsung_finals;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
     private Toolbar loginToolbar;
+    private Button loginSignUpButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -13,5 +17,11 @@ public class LoginActivity extends AppCompatActivity {
         loginToolbar = findViewById(R.id.login_toolbar);
         loginToolbar.setTitle("");
         setSupportActionBar(loginToolbar);
+        loginSignUpButton = findViewById(R.id.login_sign_up_button);
+        loginSignUpButton.setOnClickListener(listener ->
+        {
+            Intent openRegisterActivityIntent = new Intent(this,RegisterActivity.class);
+            startActivity(openRegisterActivityIntent);
+        });
     }
 }
