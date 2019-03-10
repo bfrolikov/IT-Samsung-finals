6d69775c642c3ed7f672961e1ad86e47fa7868c0
bfrolikov <b.frolikov@yandex.ru>
2019-03-10 21:27:43 +0300

    progress in firebase

--- app/src/main/java/com/example/bfrol/it_samsung_finals/LoginActivity.java
+++ app/src/main/java/com/example/bfrol/it_samsung_finals/LoginActivity.java
@@ -2,12 +2,14 @@ package com.example.bfrol.it_samsung_finals;
 
 import android.content.Intent;
 import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -33,15 +35,40 @@ public class LoginActivity extends AppCompatActivity {
         loginSignInButton.setOnClickListener(listener->{
             EditText loginEmailEdit = findViewById(R.id.login_email_edit);
             EditText loginPasswordEdit = findViewById(R.id.login_password_edit);
+            //TODO check password and email for validity
             firebaseAuth.signInWithEmailAndPassword(loginEmailEdit.getText().toString(),loginPasswordEdit.getText().toString())
                     .addOnCompleteListener(this, task -> {
+                        //TODO implement snackbar with information about the login status
                         if (task.isSuccessful())
                         {
-                            Intent openMainActivity = new Intent(this,MainActivity.class);
-                            startActivity(openMainActivity);
-                            finish();
+                            //authentication successful
+                            openMainActivity();
+                        }
+                        else
+                        {
+                            //authentication failed
+                            Toast.makeText(getApplicationContext(),"Login Error",Toast.LENGTH_LONG).show();
                         }
                     });
         });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if(firebaseAuth.getCurrentUser()!=null) // check if the user is signed in
+        {
+            openMainActivity();
+        }
+    }
+
+    private void openMainActivity()
+    {
+        Intent openMainActivity = new Intent(this,MainActivity.class);
+        openMainActivity.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);// clear activity history
+        startActivity(openMainActivity);
+        finish();
+        //login or register successful, open main activity
+
     }
 }
--- app/src/main/java/com/example/bfrol/it_samsung_finals/RegisterActivity.java
+++ app/src/main/java/com/example/bfrol/it_samsung_finals/RegisterActivity.java
@@ -1,14 +1,52 @@
 package com.example.bfrol.it_samsung_finals;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class RegisterActivity extends AppCompatActivity {
+    private FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
         //TODO add user profile image loading
+        firebaseAuth = FirebaseAuth.getInstance();
+        Button registerRegisterButton = findViewById(R.id.register_register_button);
+        registerRegisterButton.setOnClickListener(view -> {
+            EditText registerPassword = findViewById(R.id.register_password);
+            EditText registerPasswordRepeat = findViewById(R.id.register_password_repeat);
+            //TODO check password and email for validity
+            if (!registerPassword.getText().toString().equals(registerPasswordRepeat.getText().toString())) {
+                Toast.makeText(getApplicationContext(), "Passwords don't match", Toast.LENGTH_LONG).show();
+            } else {
+                EditText registerEmail = findViewById(R.id.register_email);
+                firebaseAuth.createUserWithEmailAndPassword(registerEmail.getText().toString(),registerPassword.getText().toString())
+                        .addOnCompleteListener(this,task -> {
+                            if(task.isSuccessful())
+                            {
+                                openMainActivity();
+                            }
+                            else
+                            {
+                                Toast.makeText(getApplicationContext(),"Register failed",Toast.LENGTH_LONG).show();
+                            }
+                        });
+            }
+        });
+
+    }
+    private void openMainActivity()
+    {
+        Intent openMainActivity = new Intent(this,MainActivity.class);
+        openMainActivity.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);// clear activity history
+        startActivity(openMainActivity);
+        finish();
     }
 }
